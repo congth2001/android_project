4c2b298c0af04f5454a5fa4cbd9ade57ac4e766b
NguyenThinhDatEng <thinh.nv194178@sis.hust.edu.vn>
2023-01-31 10:24:03 +0700

    update everything

--- lib/widgets/post_widget.dart
+++ lib/widgets/post_widget.dart
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
 import 'package:fakebook/models/user.dart';
-=======
-import 'package:fakebook/pages/profile_page.dart';
->>>>>>> 48b739df91fe48b1248cec1eb2772a22f63cdd3a
 import 'package:flutter/material.dart';
 import 'package:fakebook/models/post.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:like_button/like_button.dart';
 
-import '../pages/request_report_page.dart';
 import '../shared/font_size.dart';
 import 'comment_widget.dart';
 import '../network/user_request.dart';
 
-<<<<<<< HEAD
 class PostWidget extends StatefulWidget {
   Post post;
-=======
-class PostWidget extends StatelessWidget {
-  Post post;
-
->>>>>>> 48b739df91fe48b1248cec1eb2772a22f63cdd3a
   PostWidget({required this.post});
 
   @override
@@ -50,40 +39,23 @@ class _PostWidgetState extends State<PostWidget> {
   final now = DateTime.now();
 
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-          child: Row(
+    return Container(
+      padding: EdgeInsets.only(left: 15, right: 15, top: 15),
+      child: Column(
+        children: [
+          Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-<<<<<<< HEAD
               CircleAvatar(
                 backgroundImage: AssetImage(widget.post.images.toString()),
                 radius: 20.0,
-=======
-              InkWell(
-                hoverColor: Colors.white,
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ProfilePage()));
-                },
-                child: CircleAvatar(
-                  backgroundImage: AssetImage(post.profileImageUrl),
-                  radius: 20.0,
-                ),
->>>>>>> 48b739df91fe48b1248cec1eb2772a22f63cdd3a
               ),
               SizedBox(width: 7.0),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-<<<<<<< HEAD
                   Text(users.username.toString(),
-=======
-                  Text(post.username,
->>>>>>> 48b739df91fe48b1248cec1eb2772a22f63cdd3a
                       style: TextStyle(
                           fontWeight: FontWeight.bold, fontSize: 17.0)),
                   SizedBox(height: 5.0),
@@ -94,11 +66,7 @@ class _PostWidgetState extends State<PostWidget> {
               Spacer(),
               IconButton(
                   onPressed: () {
-<<<<<<< HEAD
                     if (widget.post.author == 'Sam Wilson') {
-=======
-                    if (post.username == 'Sam Wilson') {
->>>>>>> 48b739df91fe48b1248cec1eb2772a22f63cdd3a
                       showModalBottomSheet(
                           context: context,
                           builder: (BuilderContext) {
@@ -240,7 +208,6 @@ class _PostWidgetState extends State<PostWidget> {
                                             ],
                                           ),
                                         ),
-<<<<<<< HEAD
                                         SizedBox(height: 10)
                                       ],
                                     )));
@@ -269,41 +236,6 @@ class _PostWidgetState extends State<PostWidget> {
                                               SizedBox(width: 10),
                                               Text('Report post',
                                                   style: TextStyle(
-=======
-                                      ),
-                                      SizedBox(height: 10)
-                                    ],
-                                  )));});
-                  }else{
-                    showModalBottomSheet(
-                        context: context,
-                        builder: (BuilderContext) {
-                          return SizedBox(
-                              height: 100,
-                              child: Container(
-                                  padding: const EdgeInsets.all(8),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      InkWell(
-                                        hoverColor: Colors.white,
-                                        onTap: ()=>{
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(builder: (context) => RequestReportPage()),
-                                          )
-                                        },
-                                        child: Row(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.center,
-                                          children: [
-                                            Icon(Icons.report,
-                                                color: Colors.black),
-                                            SizedBox(width: 10),
-                                            Text('Report post',
-                                                style: TextStyle(
->>>>>>> 48b739df91fe48b1248cec1eb2772a22f63cdd3a
                                                     fontSize: 14,
                                                     color: Colors.black,
                                                   )),
@@ -345,29 +277,11 @@ class _PostWidgetState extends State<PostWidget> {
                   icon: Icon(Icons.more_horiz, color: Colors.black))
             ],
           ),
-<<<<<<< HEAD
           SizedBox(height: 20.0),
           Text(widget.post.described.toString(),
               style: TextStyle(fontSize: 15.0)),
           SizedBox(height: 10.0),
           Row(
-=======
-        ),
-
-        Container(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-          child: Text(post.content, style: TextStyle(fontSize: 15.0))
-        ),
-
-        Container(
-          padding: EdgeInsets.all(10),
-          child: Image.network('https://firebasestorage.googleapis.com/v0/b/social-network-app-19cd7.appspot.com/o/images%2Frn_image_picker_lib_temp_19d714d4-09ee-45a2-a1b0-c44329bcd180.jpg?alt=media&token=2ed540ab-1944-4061-b5d6-4f3ee8b598f8&fbclid=IwAR0PCicGfnvSYpF0E-dVR6bRJ40SS-NYlipPh8VQsvWHQPM7_hsh6E9aAP8', fit: BoxFit.contain, width: MediaQuery.of(context).size.width),
-        ),
-
-        Container(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-          child: Row(
->>>>>>> 48b739df91fe48b1248cec1eb2772a22f63cdd3a
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
@@ -396,7 +310,6 @@ class _PostWidgetState extends State<PostWidget> {
               ),
             ],
           ),
-<<<<<<< HEAD
           SizedBox(height: 10),
           Divider(thickness: 0.5, height: 0),
           Row(
@@ -464,65 +377,6 @@ class _PostWidgetState extends State<PostWidget> {
           )
         ],
       ),
-=======
-        ),
-        SizedBox(height: 10),
-
-        Divider(thickness: 0.5, height: 0),
-
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width/3-30,
-              height: 45,
-              child: (
-                  LikeButton(
-                    size: 60.0,
-                    circleColor:
-                    const CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
-                    bubblesColor: const BubblesColor(
-                      dotPrimaryColor: Color(0xff33b5e5),
-                      dotSecondaryColor: Color(0xff0099cc),
-                    ),
-                    likeBuilder: (isLiked) {
-                      post.likes = isLiked ? post.likes + 1 : post.likes - 1;
-                      return isLiked ? Row(children: [
-                        Icon( Icons.thumb_up,color: Colors.blue[700], size: 20.0),
-                        Text('  Like', style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, color: Colors.blue[700]))
-                      ],) : Row(children: const [
-                        Icon( Icons.thumb_up_outlined,color: Colors.black54, size: 20.0),
-                        Text('  Like', style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400))
-                      ]);
-                    },
-                  )
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width/3,
-              height: 45,
-              child: (
-                MaterialButton(
-                    onPressed: () { 
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context)=>CommentPage())
-                      );
-                     },
-                    child: Row(children: [
-                      Icon(FontAwesomeIcons.message, color: Colors.black54, size: 20.0),
-                      Text('  Comment', style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400))
-                    ],
-                      mainAxisAlignment: MainAxisAlignment.center,)
-                )
-              )
-            ),
-
-          ],
-        )
-      ],
->>>>>>> 48b739df91fe48b1248cec1eb2772a22f63cdd3a
     );
   }
 }
